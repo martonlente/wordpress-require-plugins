8c76b2cff0a72e13b87853383e4c8ab59b9f458f
Márton Lente <lentemarton@gmail.com>
2023-06-03 20:37:09 +0200

    Add js to prevent notice dismissed from re-appearing

--- inc/wordpress-require-plugins.php
+++ inc/wordpress-require-plugins.php
@@ -25,12 +25,18 @@
       $required_plugin_data = get_plugin_data(ABSPATH . 'wp-content/plugins/' . $required_plugin['file_path']);
       $required_plugin_name = $required_plugin_data['Name'];
 
+      // Init static $index for ID js notice index
+      static $index = 0;
+
       // If plugin is active, show notice success
       ?>
-      <div class="is-dismissible notice notice-success">
+      <div class="is-dismissible js-notice notice notice-success" id="<?php echo 'js-notice-' . $index; ?>">
         <p><?php _e( 'Kötelező bővítmény aktív: '. $required_plugin_name, 'wordpress-require-plugins'); ?></p>
       </div>
     <?php
+    // Increment $index
+    $index++;
+
     else:
       // Set required plugin name
       $required_plugin_name = $required_plugin['name'];
@@ -46,3 +52,8 @@
 }
 
 add_action('admin_notices', 'require_plugins');
+
+// Add js to prevent a message from re-appearing once the page re-loads, or another page is loaded
+wp_enqueue_script('wordpress-require-plugins-js-cookie', get_template_directory_uri() . '/node_modules/js-cookie/dist/js.cookie.min.js', array('jquery'), '0.1.0');
+
+wp_enqueue_script('wordpress-require-plugins-wordpress-require-plugins', get_template_directory_uri() . '/js/wordpress-require-plugins.js', array('jquery'), '0.1.0');
---
+++ js/wordpress-require-plugins.js
@@ -0,0 +1,47 @@
+/*!
+ * WordPress Require plugins v0.1.0 (https://martonlente.com/)
+ * Copyright 2023 Márton Lente
+ * Licensed under Apache 2.0 (https://github.com/martonlente/wordpress-require-plugins/blob/main/LICENSE)
+ */
+
+(function() {
+  // Put jQuery into no-conflict mode
+  jQuery.noConflict();
+
+  jQuery(document).ready(function($) {
+    // Create variable $notice
+    var $notice = $('.js-notice');
+
+    function dismissedCheck() {
+      $notice.each(function() {
+        var $this = $(this);
+        var id = $this.attr('id');
+
+        // Check if notice is dismissed
+        if (Cookies.get(id + '-is-dismissed') == 'true') {
+          // If is dismissed, hide
+          $this.css('display', 'none');
+        }
+      });
+    }
+
+    function dismissedSet() {
+      // Create event $notice on click
+      $notice.on('click', function() {
+        var id = $(this).attr('id');
+
+        // Create cookie id -is-dismissed on click for 7 days
+        Cookies.set(id + '-is-dismissed', 'true', {expires: 7});
+      });
+    }
+
+    function init() {
+      // Call functions dismissed
+      dismissedCheck();
+      dismissedSet();
+    }
+
+    // Call init when DOM is ready
+    init();
+  });
+}());
